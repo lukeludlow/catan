c421d6f427144bbd683bbafb987d176679af32a6
lukeludlow <lukeludlow@live.com>
2020-05-01 11:54:44 -0700

    karma config

--- src/app/_services/map-display.service.ts
+++ src/app/_services/map-display.service.ts
@@ -4,5 +4,15 @@ import { Injectable } from "@angular/core";
     providedIn: "root",
 })
 export class MapDisplayService {
+    private gameOption = "base";
+
     constructor() {}
+
+    setGameOption(gameOption: string): void {
+        this.gameOption = gameOption;
+    }
+
+    generate(): void {
+        console.log("map display service generate: " + this.gameOption);
+    }
 }

f5054fd0820750851d295a4c159f2432febd58b5
lukeludlow <lukeludlow@live.com>
2020-05-02 16:57:48 -0700

    diagonal collisions

--- src/app/_services/board-generator.service.spec.ts
+++ src/app/_services/board-generator.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from "@angular/core/testing";
 
 import { BoardGeneratorService } from "./board-generator.service";
+import { Hex } from "./Hex";
 
 describe("BoardGeneratorService", () => {
     let service: BoardGeneratorService;
@@ -24,7 +25,7 @@ describe("BoardGeneratorService", () => {
         expect(hexes[4].length).toEqual(5);
     });
 
-    it("generate should return 6 null hexes", () => {
+    it("generate should return 6 empty hexes", () => {
         const hexes = service.generate();
         let count = 0;
         hexes.forEach((row) =>
@@ -41,10 +42,14 @@ describe("BoardGeneratorService", () => {
         const hexes = service.generate();
         const resourceNames = ["rock", "wheat", "brick", "tree", "sheep"];
         let numResourceHexes = 0;
-        console.log(hexes);
         hexes.forEach((row) =>
             row.forEach((hex) => {
-                if (hex.resource && hex.resource !== "" && resourceNames.includes(hex.resource)) {
+                if (hex.resource === null || hex.resource === undefined || hex.resource === "") {
+                    // continue;
+                    // do nothing
+                    // } else if (resourceNames.includes(hex.resource)) {
+                } else if (resourceNames.indexOf(hex.resource) !== -1) {
+                    // else if (hex.resource && hex.resource !== "" && resourceNames.includes(hex.resource)) {
                     numResourceHexes = numResourceHexes + 1;
                 }
             })
@@ -52,16 +57,237 @@ describe("BoardGeneratorService", () => {
         expect(numResourceHexes).toEqual(18);
     });
 
-    it("generate should return one desert hex", () => {
+    it("generate should create correct amount of each resource", () => {
         const hexes = service.generate();
-        let numDesertTilesFound = 0;
+        const resourceTilesFound = new Map<string, number>();
+        resourceTilesFound.set("rock", 0);
+        resourceTilesFound.set("wheat", 0);
+        resourceTilesFound.set("brick", 0);
+        resourceTilesFound.set("tree", 0);
+        resourceTilesFound.set("sheep", 0);
+        resourceTilesFound.set("desert", 0);
         hexes.forEach((row) =>
             row.forEach((hex) => {
-                if (hex.resource === "desert") {
-                    numDesertTilesFound++;
+                resourceTilesFound.set(hex.resource, resourceTilesFound.get(hex.resource) + 1);
+            })
+        );
+        expect(resourceTilesFound.get("rock")).toEqual(3);
+        expect(resourceTilesFound.get("wheat")).toEqual(4);
+        expect(resourceTilesFound.get("brick")).toEqual(3);
+        expect(resourceTilesFound.get("tree")).toEqual(4);
+        expect(resourceTilesFound.get("sheep")).toEqual(4);
+        expect(resourceTilesFound.get("desert")).toEqual(1);
+    });
+
+    it("generate should assign dice numbers to each resource hex", () => {
+        const hexes = service.generate();
+        hexes.forEach((row) =>
+            row.forEach((hex) => {
+                if (hex.resource !== "" && hex.resource !== "desert") {
+                    expect(hex.diceNumber).toBeGreaterThan(1);
+                    expect(hex.diceNumber).toBeLessThan(13);
                 }
             })
         );
-        expect(numDesertTilesFound).toEqual(1);
+    });
+
+    it("generate should create correct amount of each diceNumber", () => {
+        const hexes = service.generate();
+        const diceNumbersFound = new Map<number, number>();
+        diceNumbersFound.set(2, 0);
+        diceNumbersFound.set(3, 0);
+        diceNumbersFound.set(4, 0);
+        diceNumbersFound.set(5, 0);
+        diceNumbersFound.set(6, 0);
+        diceNumbersFound.set(8, 0);
+        diceNumbersFound.set(9, 0);
+        diceNumbersFound.set(10, 0);
+        diceNumbersFound.set(11, 0);
+        diceNumbersFound.set(12, 0);
+        hexes.forEach((row) =>
+            row.forEach((hex) => {
+                if (hex.resource !== "" && hex.resource !== "desert") {
+                    diceNumbersFound.set(hex.diceNumber, diceNumbersFound.get(hex.diceNumber) + 1);
+                }
+            })
+        );
+        expect(diceNumbersFound.get(2)).toEqual(1);
+        expect(diceNumbersFound.get(3)).toEqual(2);
+        expect(diceNumbersFound.get(4)).toEqual(2);
+        expect(diceNumbersFound.get(5)).toEqual(2);
+        expect(diceNumbersFound.get(6)).toEqual(2);
+        expect(diceNumbersFound.get(8)).toEqual(2);
+        expect(diceNumbersFound.get(9)).toEqual(2);
+        expect(diceNumbersFound.get(10)).toEqual(2);
+        expect(diceNumbersFound.get(11)).toEqual(2);
+        expect(diceNumbersFound.get(12)).toEqual(1);
+    });
+
+    it("areHexesSixEightCollision no assigned dice numbers should return false", () => {
+        const gridSize = 5;
+        const hexes = new Array<Array<Hex>>();
+        for (let row = 0; row < gridSize; row++) {
+            hexes[row] = new Array<Hex>(gridSize);
+            for (let col = 0; col < gridSize; col++) {
+                hexes[row][col] = new Hex(-1, -1, -1, "");
+            }
+        }
+        const hex1 = hexes[2][1];
+        const hex2 = hexes[2][2];
+        const isCollision = service.areHexesSixEightCollision(hex1, hex2);
+        expect(isCollision).toBeFalse();
+    });
+
+    it("areHexesSixEightCollision should detect horizontal collision", () => {
+        const gridSize = 5;
+        const hexes = new Array<Array<Hex>>();
+        for (let row = 0; row < gridSize; row++) {
+            hexes[row] = new Array<Hex>(gridSize);
+            for (let col = 0; col < gridSize; col++) {
+                hexes[row][col] = new Hex(-1, -1, -1, "");
+            }
+        }
+        hexes[2][1] = new Hex(2, 1, 6, "rock");
+        hexes[2][2] = new Hex(2, 2, 8, "rock");
+        const hex1 = hexes[2][1];
+        const hex2 = hexes[2][2];
+        const isCollision = service.areHexesSixEightCollision(hex1, hex2);
+        expect(isCollision).toBeTrue();
+    });
+
+    it("areHexesSixEightCollision should detect diagonal collision bottom left even to odd row", () => {
+        const gridSize = 5;
+        const hexes = new Array<Array<Hex>>();
+        for (let row = 0; row < gridSize; row++) {
+            hexes[row] = new Array<Hex>(gridSize);
+            for (let col = 0; col < gridSize; col++) {
+                hexes[row][col] = new Hex(-1, -1, -1, "");
+            }
+        }
+        hexes[0][2] = new Hex(0, 2, 6, "rock");
+        hexes[1][2] = new Hex(1, 2, 8, "rock");
+        const hex1 = hexes[0][2];
+        const hex2 = hexes[1][2];
+        const isCollision = service.areHexesSixEightCollision(hex1, hex2);
+        expect(isCollision).toBeTrue();
+    });
+
+    it("areHexesSixEightCollision should detect diagonal collision bottom left odd to even row", () => {
+        const gridSize = 5;
+        const hexes = new Array<Array<Hex>>();
+        for (let row = 0; row < gridSize; row++) {
+            hexes[row] = new Array<Hex>(gridSize);
+            for (let col = 0; col < gridSize; col++) {
+                hexes[row][col] = new Hex(-1, -1, -1, "");
+            }
+        }
+        hexes[1][2] = new Hex(1, 2, 6, "rock");
+        hexes[2][1] = new Hex(2, 1, 8, "rock");
+        const hex1 = hexes[1][2];
+        const hex2 = hexes[2][1];
+        const isCollision = service.areHexesSixEightCollision(hex1, hex2);
+        expect(isCollision).toBeTrue();
+    });
+
+    it("areHexesSixEightCollision should detect diagonal collision bottom right even to odd row", () => {
+        const gridSize = 5;
+        const hexes = new Array<Array<Hex>>();
+        for (let row = 0; row < gridSize; row++) {
+            hexes[row] = new Array<Hex>(gridSize);
+            for (let col = 0; col < gridSize; col++) {
+                hexes[row][col] = new Hex(-1, -1, -1, "");
+            }
+        }
+        hexes[0][3] = new Hex(0, 3, 6, "rock");
+        hexes[1][3] = new Hex(1, 3, 8, "rock");
+        const hex1 = hexes[0][3];
+        const hex2 = hexes[1][3];
+        const isCollision = service.areHexesSixEightCollision(hex1, hex2);
+        expect(isCollision).toBeTrue();
+    });
+
+    it("areHexesSixEightCollision should detect diagonal collision bottom right odd to even row", () => {
+        const gridSize = 5;
+        const hexes = new Array<Array<Hex>>();
+        for (let row = 0; row < gridSize; row++) {
+            hexes[row] = new Array<Hex>(gridSize);
+            for (let col = 0; col < gridSize; col++) {
+                hexes[row][col] = new Hex(-1, -1, -1, "");
+            }
+        }
+        hexes[1][4] = new Hex(1, 4, 6, "rock");
+        hexes[2][4] = new Hex(2, 4, 8, "rock");
+        const hex1 = hexes[1][4];
+        const hex2 = hexes[2][4];
+        const isCollision = service.areHexesSixEightCollision(hex1, hex2);
+        expect(isCollision).toBeTrue();
+    });
+
+    it("areHexesSixEightCollision should detect diagonal collision top left even to odd row", () => {
+        const gridSize = 5;
+        const hexes = new Array<Array<Hex>>();
+        for (let row = 0; row < gridSize; row++) {
+            hexes[row] = new Array<Hex>(gridSize);
+            for (let col = 0; col < gridSize; col++) {
+                hexes[row][col] = new Hex(-1, -1, -1, "");
+            }
+        }
+        hexes[4][1] = new Hex(4, 1, 6, "rock");
+        hexes[3][1] = new Hex(3, 1, 8, "rock");
+        const hex1 = hexes[4][1];
+        const hex2 = hexes[3][1];
+        const isCollision = service.areHexesSixEightCollision(hex1, hex2);
+        expect(isCollision).toBeTrue();
+    });
+
+    it("areHexesSixEightCollision should detect diagonal collision top left odd to even row", () => {
+        const gridSize = 5;
+        const hexes = new Array<Array<Hex>>();
+        for (let row = 0; row < gridSize; row++) {
+            hexes[row] = new Array<Hex>(gridSize);
+            for (let col = 0; col < gridSize; col++) {
+                hexes[row][col] = new Hex(-1, -1, -1, "");
+            }
+        }
+        hexes[1][1] = new Hex(1, 1, 6, "rock");
+        hexes[0][1] = new Hex(0, 1, 8, "rock");
+        const hex1 = hexes[1][1];
+        const hex2 = hexes[0][1];
+        const isCollision = service.areHexesSixEightCollision(hex1, hex2);
+        expect(isCollision).toBeTrue();
+    });
+
+    it("areHexesSixEightCollision should detect diagonal collision top right even to odd row", () => {
+        const gridSize = 5;
+        const hexes = new Array<Array<Hex>>();
+        for (let row = 0; row < gridSize; row++) {
+            hexes[row] = new Array<Hex>(gridSize);
+            for (let col = 0; col < gridSize; col++) {
+                hexes[row][col] = new Hex(-1, -1, -1, "");
+            }
+        }
+        hexes[2][2] = new Hex(2, 2, 6, "rock");
+        hexes[1][3] = new Hex(1, 3, 8, "rock");
+        const hex1 = hexes[2][2];
+        const hex2 = hexes[1][3];
+        const isCollision = service.areHexesSixEightCollision(hex1, hex2);
+        expect(isCollision).toBeTrue();
+    });
+
+    it("areHexesSixEightCollision should detect diagonal collision top right odd to even row", () => {
+        const gridSize = 5;
+        const hexes = new Array<Array<Hex>>();
+        for (let row = 0; row < gridSize; row++) {
+            hexes[row] = new Array<Hex>(gridSize);
+            for (let col = 0; col < gridSize; col++) {
+                hexes[row][col] = new Hex(-1, -1, -1, "");
+            }
+        }
+        hexes[3][4] = new Hex(3, 4, 6, "rock");
+        hexes[2][4] = new Hex(2, 4, 8, "rock");
+        const hex1 = hexes[3][4];
+        const hex2 = hexes[2][4];
+        const isCollision = service.areHexesSixEightCollision(hex1, hex2);
+        expect(isCollision).toBeTrue();
     });
 });
--- src/app/_services/board-generator.service.ts
+++ src/app/_services/board-generator.service.ts
@@ -9,6 +9,7 @@ import { Hex } from "./Hex";
 })
 export class BoardGeneratorService {
     private tiles: Map<string, number>;
+    private diceNumbers: Map<number, number>;
 
     constructor() {
         this.tiles = new Map<string, number>();
@@ -18,6 +19,73 @@ export class BoardGeneratorService {
         this.tiles.set("tree", 4);
         this.tiles.set("sheep", 4);
         this.tiles.set("desert", 1);
+        this.diceNumbers = new Map<number, number>();
+        this.diceNumbers.set(2, 1);
+        this.diceNumbers.set(3, 2);
+        this.diceNumbers.set(4, 2);
+        this.diceNumbers.set(5, 2);
+        this.diceNumbers.set(6, 2);
+        this.diceNumbers.set(8, 2);
+        this.diceNumbers.set(9, 2);
+        this.diceNumbers.set(10, 2);
+        this.diceNumbers.set(11, 2);
+        this.diceNumbers.set(12, 1);
+    }
+
+    public areHexesSixEightCollision(hex1: Hex, hex2: Hex): boolean {
+        if (hex1.diceNumber === -1 || hex2.diceNumber === -1) {
+            return false;
+        }
+        if (hex1.diceNumber !== 6 && hex1.diceNumber !== 8) {
+            return false;
+        }
+        if (hex2.diceNumber !== 6 && hex2.diceNumber !== 8) {
+            return false;
+        }
+        if (hex1.row === hex2.row && hex1.col === hex2.col) {
+            // it's the same hex
+            return false;
+        }
+        const oddrDirections = {
+            odd: [
+                [1, 0],
+                [1, -1],
+                [0, -1],
+                [-1, 0],
+                [0, 1],
+                [1, 1],
+            ],
+            even: [
+                [1, 0],
+                [0, -1],
+                [-1, -1],
+                [-1, 0],
+                [-1, 1],
+                [0, 1],
+            ],
+        };
+        console.log("hex1: " + JSON.stringify(hex1));
+        console.log("hex2: " + JSON.stringify(hex2));
+        const isOddRow = hex1.row % 2 === 1;
+        let neighborDirections: number[][];
+        if (isOddRow) {
+            // hex1col = hex1col + 1;
+            console.log("is odd row");
+            neighborDirections = oddrDirections.odd;
+        } else {
+            console.log("is even row");
+            neighborDirections = oddrDirections.even;
+        }
+        console.log("hex1 coords: " + hex1.row + "," + hex1.col);
+        for (const direction of neighborDirections) {
+            const neighborRow = hex1.row + direction[0];
+            const neighborCol = hex1.col + direction[1];
+            console.log("checking neighbor coords: " + neighborRow + "," + neighborCol);
+            if (neighborRow === hex2.row && neighborCol === hex2.col) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public generate(): Hex[][] {
@@ -40,6 +108,13 @@ export class BoardGeneratorService {
             }
         }
         // assign dice numbers
+        for (let row = 0; row < gridSize; row++) {
+            for (let col = 0; col < gridSize; col++) {
+                if (hexes[row][col].resource !== "" && hexes[row][col].resource !== "desert") {
+                    hexes[row][col].diceNumber = this.assignDiceNumber();
+                }
+            }
+        }
         return hexes;
     }
 
@@ -54,6 +129,19 @@ export class BoardGeneratorService {
         const assignedResource = [...remainingTileResources][randomIndex];
         this.tiles.set(assignedResource, this.tiles.get(assignedResource) - 1);
         return assignedResource;
+    }
+
+    private assignDiceNumber(): number {
+        const remainingDiceNumbers = new Set<number>();
+        this.diceNumbers.forEach((numRemaining, diceNumber) => {
+            if (numRemaining > 0) {
+                remainingDiceNumbers.add(diceNumber);
+            }
+        });
+        const randomIndex = Math.floor(Math.random() * remainingDiceNumbers.size);
+        const assignedDiceNumber = [...remainingDiceNumbers][randomIndex];
+        this.diceNumbers.set(assignedDiceNumber, this.diceNumbers.get(assignedDiceNumber) - 1);
+        return assignedDiceNumber;
     }
 
     private shouldCreateHexForPosition(row: number, col: number): boolean {
